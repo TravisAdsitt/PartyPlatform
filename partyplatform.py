6dd0fa10a8230e5b5032532dfc84bb3d5dfed247
TravisAdsitt <70sRocker@gmail.com>
2022-12-25 22:34:06 -0700

    Article Publish Commit

--- partyplatform.py
+++ partyplatform.py
@@ -1,10 +1,14 @@
-from kivy_garden.graph import Graph, MeshLinePlot
+from typing import List
+from kivy_garden.graph import Graph, MeshLinePlot, LinePlot
 from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
 from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.app import App
+from kivy.uix.label import Label
 import random
 from simulationobjects import World
 
@@ -12,77 +16,247 @@ from simulationobjects import World
 class PopulationGraph(RelativeLayout):
     pop_graph = ObjectProperty(None)
 
-    def __init__(self, num_towns: int):
+    def __init__(self, town_colors: List[List[int]]):
         super().__init__()
-        randint = random.randint
+        num_towns = len(town_colors)
 
         self.population_lists = [[] for _ in range(num_towns)]
-        self.plots = [MeshLinePlot(color=[randint(0,100) / 100, randint(0,100) / 100, randint(0,100) / 100, 1]) for _ in range(num_towns)]
-        self.moved_list = []
+        self.plots = [LinePlot(color=town_color, line_width=2) for town_color in town_colors]
 
         for plot in self.plots:
             self.pop_graph.add_plot(plot)
 
-        Clock.schedule_interval(self.update_graph, 1/30)
         
 
-    def update_graph(self, dt):
+    def update_graph(self):
+        max_y = 0
+        min_y = -1
         for i, plot in enumerate(self.plots):
-            start_index = 0 if len(self.population_lists[i]) < 20 else -10
+            start_index = 0 # if len(self.population_lists[i]) < 100 else -100
             # Assemble and set the points
             plot.points = [(i, pop) for i, pop in enumerate(self.population_lists[i][start_index:])]
-            # Extend the plots max x
-            self.pop_graph.xmax = len(self.population_lists[i][start_index:]) + 1
+            max_y = max(max_y, max(self.population_lists[i]))
+            if min_y < 0:
+                min_y = min(self.population_lists[i])
+            else:
+                min_y = min(min_y, min(self.population_lists[i]))
+        
+        self.pop_graph.xmax = len(self.population_lists[0][start_index:]) + 1
+        self.pop_graph.x_ticks_major = self.pop_graph.xmax // 10
+        self.pop_graph.x_ticks_minor = self.pop_graph.xmax // 100
+        self.pop_graph.ymax = max_y + 10
+        self.pop_graph.ymin = min_y - 10
 
-class PeopleMovedGraph(RelativeLayout):
+
+class PeopleMovedGraph(BoxLayout):
     mov_graph = ObjectProperty(None)
+    want_mov_graph = ObjectProperty(None)
 
     def __init__(self):
         super().__init__()
+        self.orientation = "vertical"
         randint = random.randint
 
-        self.plot = MeshLinePlot(color=[randint(0,100) / 100, randint(0,100) / 100, randint(0,100) / 100, 1])
+        self.moved_plot = LinePlot(color=[randint(0,100) / 100, randint(0,100) / 100, randint(0,100) / 100, 1], line_width=2)
+        self.want_to_move_plot = LinePlot(color=[randint(0,100) / 100, randint(0,100) / 100, randint(0,100) / 100, 1], line_width=2)
+
+        self.want_to_move_list = []
         self.moved_list = []
 
-        self.mov_graph.add_plot(self.plot)
-
-        Clock.schedule_interval(self.update_graph, 1/30)
+        self.mov_graph.add_plot(self.moved_plot)
+        self.want_mov_graph.add_plot(self.want_to_move_plot)
         
-
-    def update_graph(self, dt):
-        start_index = 0 if len(self.moved_list) < 20 else -10
+    def update_graph(self):
+        start_index = 0 # if len(self.moved_list) < 20 else -20
         # Assemble and set the points
-        self.plot.points = [(i, num_moved) for i, num_moved in enumerate(self.moved_list[start_index:])]
+        self.moved_plot.points = [(i, num_moved) for i, num_moved in enumerate(self.moved_list[start_index:])]
+        self.want_to_move_plot.points = [(i, num_want_moved) for i, num_want_moved in enumerate(self.want_to_move_list[start_index:])]
+
+        self.want_mov_graph.xmax = len(self.want_to_move_list[start_index:]) + 1
+        self.want_mov_graph.x_ticks_major = len(self.want_to_move_list[start_index:]) // 10
+        self.want_mov_graph.x_ticks_minor = len(self.want_to_move_list[start_index:]) // 100
+        self.want_mov_graph.ymax = max(self.want_to_move_list[start_index:]) + 10
+        self.want_mov_graph.ymin = min(self.want_to_move_list[start_index:]) - 10
+
+        self.mov_graph.xmax = len(self.moved_list[start_index:]) + 1
+        self.mov_graph.x_ticks_major = len(self.moved_list[start_index:]) // 10
+        self.mov_graph.x_ticks_minor = len(self.moved_list[start_index:]) // 100
+        self.mov_graph.ymax = max(self.moved_list[start_index:]) + 10
+        self.mov_graph.ymin = min(self.moved_list[start_index:])- 10
+
+class TownWealthGraph(RelativeLayout):
+    pop_graph = ObjectProperty(None)
+
+    def __init__(self, town_colors: List[List[int]]):
+        super().__init__()
+        num_towns = len(town_colors)
+
+        self.wealth_lists = [[] for _ in range(num_towns)]
+        self.plots = [LinePlot(color=town_color, line_width=2) for town_color in town_colors]
+
+        for plot in self.plots:
+            self.wealth_graph.add_plot(plot)
+
+    def update_graph(self):
+        y_max = 0
+        for i, plot in enumerate(self.plots):
+            start_index = 0 # if len(self.population_lists[i]) < 100 else -100
+            # Assemble and set the points
+            plot.points = [(i, pop) for i, pop in enumerate(self.wealth_lists[i][start_index:])]
+            y_max_temp = max(self.wealth_lists[i][start_index:])
+            if y_max_temp > y_max:
+                y_max = y_max_temp
 
         # Extend the plots max x
-        self.mov_graph.xmax = len(self.moved_list[start_index:]) + 1
-        self.mov_graph.ymax = max(self.moved_list[start_index:]) + 10
-        self.mov_graph.ymin = min(self.moved_list[start_index:]) - 10
+        self.wealth_graph.xmax = len(self.wealth_lists[0][start_index:]) + 1
+        self.wealth_graph.x_ticks_major = self.wealth_graph.xmax // 10
+        self.wealth_graph.x_ticks_minor = self.wealth_graph.xmax // 100
+
+        self.wealth_graph.ymax = y_max
+        self.wealth_graph.y_ticks_major = y_max // 10
+        self.wealth_graph.y_ticks_minor = y_max // 100
+
+class AverageWealthGraph(RelativeLayout):
+    avg_wealth_graph = ObjectProperty(None)
+
+    def __init__(self, town_colors: List[List[int]]):
+        super().__init__()
+        num_towns = len(town_colors)
+
+        self.wealth_lists = [[] for _ in range(num_towns)]
+        self.plots = [LinePlot(color=town_color, line_width=2) for town_color in town_colors]
+
+        for plot in self.plots:
+            self.avg_wealth_graph.add_plot(plot)
+
+    def update_graph(self):
+        y_max = 0
+        for i, plot in enumerate(self.plots):
+            start_index = 0 # if len(self.population_lists[i]) < 100 else -100
+            # Assemble and set the points
+            plot.points = [(i, pop) for i, pop in enumerate(self.wealth_lists[i][start_index:])]
+            y_max_temp = max(self.wealth_lists[i][start_index:])
+            if y_max_temp > y_max:
+                y_max = y_max_temp
+
+        # Extend the plots max x
+        self.avg_wealth_graph.xmax = len(self.wealth_lists[0][start_index:]) + 1
+        self.avg_wealth_graph.x_ticks_major = self.avg_wealth_graph.xmax // 10
+        self.avg_wealth_graph.x_ticks_minor = self.avg_wealth_graph.xmax // 100
+
+        self.avg_wealth_graph.ymax = y_max
+        self.avg_wealth_graph.y_ticks_major = y_max // 10
+        self.avg_wealth_graph.y_ticks_minor = y_max // 100
+
+class MarketDemandGraph(RelativeLayout):
+    market_dmd_graph = ObjectProperty(None)
+
+    def __init__(self, town_colors: List[List[int]]):
+        super().__init__()
+        num_towns = len(town_colors)
+
+        self.demand_lists = [[] for _ in range(num_towns)]
+        self.plots = [LinePlot(color=town_color, line_width=2) for town_color in town_colors]
+
+        for plot in self.plots:
+            self.market_dmd_graph.add_plot(plot)
+
+    def update_graph(self):
+        y_max = 0
+        
+        for i, plot in enumerate(self.plots):
+            start_index = 0 # if len(self.population_lists[i]) < 100 else -100
+            # Assemble and set the points
+            plot.points = [(i, pop) for i, pop in enumerate(self.demand_lists[i][start_index:])]
+            y_max_temp = max(self.demand_lists[i][start_index:])
+            if y_max_temp > y_max:
+                y_max = y_max_temp
+
+        # Extend the plots max x
+        self.market_dmd_graph.xmax = len(self.demand_lists[0][start_index:]) + 1
+        self.market_dmd_graph.x_ticks_major = self.market_dmd_graph.xmax // 10
+        self.market_dmd_graph.x_ticks_minor = self.market_dmd_graph.xmax // 100
+
+        self.market_dmd_graph.ymax = y_max
+        self.market_dmd_graph.y_ticks_major = y_max // 10
+        self.market_dmd_graph.y_ticks_minor = y_max // 100
+
+class ControlPanel(BoxLayout):
+    def __init__(self, start_callback, kwargs):
+        super().__init__(**kwargs)
+        self.start_callback = start_callback
+        self.sim_started = False
+
+    def start_stop_sim(self):
+        self.sim_started = not self.sim_started
+
+        if self.sim_started:
+            self.ids.go_button.text = "Stop"
+            Clock.schedule_interval(self.start_callback, 1/10)
+        else:
+            self.ids.go_button.text = "Go"
+            
+        
 
 class PartyPlatformApp(App):
+    go_button = Button()
+
     def __init__(self):
         super().__init__()
         self.world = World()
-        Clock.schedule_interval(self.step_sim, 1/60)
+        self.update_graphs_dt = 0
 
     def step_sim(self, dt):
         step_info = self.world.step_world()
+        self.update_graphs_dt += dt
 
         for town_index in range(len(self.world.towns)):
             self.population_graph.population_lists[town_index].append(len(self.world.towns[town_index].people))
+            self.wealth_graph.wealth_lists[town_index].append(self.world.towns[town_index].get_total_wealth())
 
         self.movers_graph.moved_list.append(step_info.number_people_moved)
+        self.movers_graph.want_to_move_list.append(step_info.number_people_desire_moved)
 
+        for i, mc in enumerate(step_info.current_market.moving_costs):
+            self.town_moving_cost_graph.demand_lists[i].append(mc)
+        
+
+        if self.control_panel.sim_started and self.update_graphs_dt > 0.5:
+            self.population_graph.update_graph()
+            self.movers_graph.update_graph()
+            self.town_moving_cost_graph.update_graph()
+            self.wealth_graph.update_graph()
+            self.update_graphs_dt = 0
+        else:
+            return self.control_panel.sim_started
+    
+    @property
+    def town_colors(self):
+        if not hasattr(self, "_town_colors"):
+            randint = random.randint
+            self._town_colors = [[randint(0,100) / 100, randint(0,100) / 100, randint(0,100) / 100, 1] for _ in range(len(self.world.towns))]
+        
+        return self._town_colors
+    
     def build(self):
-        grid = GridLayout(cols=2)
+        root_grid = GridLayout(rows=2 + len(self.world.towns))
+        graph_grid = GridLayout(cols=2)
 
-        self.population_graph = PopulationGraph(len(self.world.towns))
+        self.population_graph = PopulationGraph(self.town_colors)
+        self.wealth_graph = TownWealthGraph(self.town_colors)
+        self.town_moving_cost_graph = MarketDemandGraph(self.town_colors)
         self.movers_graph = PeopleMovedGraph()
+        self.control_panel = ControlPanel(self.step_sim, {"size_hint":(1, 0.3)})
 
-        grid.add_widget(self.population_graph)
-        grid.add_widget(self.movers_graph)
+        graph_grid.add_widget(self.population_graph)
+        graph_grid.add_widget(self.movers_graph)
+        graph_grid.add_widget(self.wealth_graph)
+        graph_grid.add_widget(self.town_moving_cost_graph)
+        root_grid.add_widget(graph_grid)
 
-        return grid
+        root_grid.add_widget(self.control_panel)
+
+        return root_grid
 
 if __name__ == "__main__":
     PartyPlatformApp().run()
--- simulationobjects.py
+++ simulationobjects.py
@@ -5,23 +5,40 @@
 #   Author: Travis Adsitt
 ##
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import random
+from typing import List
 
+
+class IDManager:
+    id = 0
+    def __new__(cls):
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(IDManager, cls).__new__(cls)
+            return cls.instance
+    
+    @classmethod
+    def getNewID(cls):
+        cls.id += 1
+        
+        return cls.id
+        
 
 class Person:
     def __init__(self, preference_width: int, move_threshold: float = None):
+        self.id = IDManager.getNewID()
         self.preference_width = preference_width
         # UINT8 bitfield for preferences
         self.preference_base_bits = random.getrandbits(self.preference_width) 
         # Are we basically happy or sad?
         self.basically_happy = bool(random.getrandbits(1))
-        # Set a random threshold in a range if None
+        # Set a random happiness threshold in a range if None
         if move_threshold == None:
             self.move_threshold = random.randint(40, 60) / 100
         else:
             self.move_threshold = move_threshold
-
+        # Set persons money
+        self.money = 0
 
         # Setup our unbendables
         self._set_unbendable_preferences_()
@@ -61,29 +78,112 @@ class Person:
                 if preference_bit == environment_bit:
                     hapinness += 1
         
+        if hapinness > self.preference_width:
+            hapinness = self.preference_width
+        elif hapinness < 0:
+            hapinness = 0
+        
         return hapinness
     
-    def wants_to_move(self, environment_value: int):
-        happinness = self.check_happiness(environment_value) / self.preference_width
+    def is_happy(self, environment_value: int):
+        return bool((self.check_happiness(environment_value) / 8) > self.move_threshold)
 
-        return bool(happinness < self.move_threshold)
+    def wants_to_move(self, from_env: int, to_env: int):
+        happinness_from = self.check_happiness(from_env) / self.preference_width
+        happinness_to = self.check_happiness(to_env) / self.preference_width
 
+        return bool(happinness_from < self.move_threshold) and bool(happinness_to > happinness_from)
 
+    def can_move(self, cost: int = 20):
+        return bool(self.money - cost > 0)
+
+@dataclass
+class TownPersonStatus:
+    earned_ytd: int
+    seniority: float
 
 class Town:
-    def __init__(self, town_pop_max: int, town_pop_min: int = 0, platform_width: int = 32):
-        self.population = random.randint(town_pop_min, town_pop_max)
+    def __init__(self, town_pop_max: int, town_pop_min: int = 0, platform_width: int = 8, starting_wealth_per_person: int = 100, utility_rate: int = 0.1, income_tax_rate: int = 0.36):
+        
+        self.starting_population = random.randint(town_pop_min, town_pop_max)
+        self.id = IDManager.getNewID()
         self.platform_width = platform_width
+        self.starting_wealth_per_person = starting_wealth_per_person
+        self.starting_bank = self.starting_population * self.starting_wealth_per_person
+        self.town_bank = self.starting_bank
+        self.income_town_tax_rate = income_tax_rate
+        self.town_utility_rate = utility_rate
+        self.people_status = {}
         self.people = []
+        
 
-        self._init_population_()
+        self._init_population_(self.starting_population)
         self.vote_for_platform()
+    
+    @property
+    def utility_per_person(self):
+        return self.average_wealth * self.town_utility_rate
 
+    @property
+    def population(self):
+        return len(self.people)
+
+    def _init_population_(self, initial_population: int):
+        for _ in range(initial_population):
+            new_person = Person(self.platform_width)
+            # Keep track of how long people have lived here
+            self.people_status[new_person.id] = TownPersonStatus(0, random.randint(0, 25) / 100)
+            self.people.append(new_person)
     
-    def _init_population_(self):
-        for _ in range(self.population):
-            self.people.append(Person(self.platform_width))
+    def get_person_with_id(self, id: int):
+        for person in self.people:
+            if person.id == id:
+                return person
+        
+        return None
+
+    def calculate_base_rate_taxes(self, person_status: TownPersonStatus):
+        taxes = self.income_town_tax_rate * person_status.earned_ytd
+        utilities = self.utility_per_person * person_status.seniority
+
+        return taxes + utilities
+
+    def step_town(self, tax_step: bool = False):
+        for person in self.people:
+            if person.id not in self.people_status:
+                self.people_status[person.id] = TownPersonStatus(0, 0)
+
+            if self.people_status[person.id].seniority < 1:
+                self.people_status[person.id].seniority += 0.01
+
+        random_order_keys = list(self.people_status.keys())
+        random.shuffle(random_order_keys)
+
+        for person in random_order_keys:
+            person_obj = self.get_person_with_id(person)
+            
+            if person_obj is None:
+                continue
+
+            step_loss = min(self.town_bank, self.people_status[person].seniority)
+            tax_calculation = self.calculate_base_rate_taxes(self.people_status[person]) * (person_obj.money / self.get_total_wealth())
+
+            self.people_status[person].earned_ytd += step_loss if not tax_step else 0
+            person_obj.money += step_loss if not tax_step else -tax_calculation
+            self.town_bank -= step_loss if not tax_step else -tax_calculation
+
+            if tax_step:
+                self.people_status[person].earned_ytd = 0
+            
+
+
+    def get_total_wealth(self, include_bank: bool = True):
+        return round(sum([person.money for person in self.people]), 2) + (self.town_bank if include_bank else 0)
     
+    @property
+    def average_wealth(self):
+        return round(self.get_total_wealth(False) / len(self.people), 2)
+
     def vote_for_platform(self):
         if not self.people:
             return
@@ -108,15 +208,12 @@ class Town:
                 platform |= 1 << bit
         
         self.town_platform = platform
-    
-    def add_person(self, new_person: Person):
-        self.people.append(new_person)
 
     def get_movers(self):
         movers = []
 
         for person in self.people:
-            if person.wants_to_move(self.town_platform):
+            if not person.is_happy(self.town_platform):
                 movers.append(person)
 
         for mover in movers:
@@ -124,69 +221,125 @@ class Town:
         
         return movers
 
-    def visit(self, person: Person):
-        return person.check_happiness(self.town_platform)
+@dataclass
+class MovingGroup:
+    from_town: Town
+    people: List[Person]
 
-    def __str__(self):
-        return f"TOWN PLATFORM: {bin(self.town_platform)[2:]} ({hex(self.town_platform)}) TOWN POPULATION: {len(self.people)}\n"
+@dataclass
+class TownDemand:
+    town: Town
+    num_people_want: int = 0
+    num_people_leaving: int = 0
 
+    def get_move_cost_adjustment(self):
+        total_movement = self.num_people_want + self.num_people_leaving
+        movement_delta = self.num_people_want - self.num_people_leaving
+        return 1 + (movement_delta / total_movement)
+
+@dataclass
+class MovingMarket:
+    base_moving_cost: int
+    town_demand: List[TownDemand] = field(default_factory=list)
+
+    @property
+    def moving_costs(self):
+        town_moving_costs = []
+
+        for td in self.town_demand:
+            town_moving_costs.append(self.get_town_moving_cost(td.town))
+        
+        return town_moving_costs
+
+    def get_town_moving_cost(self, town: Town):
+        for td in self.town_demand:
+            if td.town == town:
+                return max(self.base_moving_cost * td.get_move_cost_adjustment(), self.base_moving_cost)
 
 @dataclass
 class WorldStepInfo:
     number_people_moved: int
+    number_people_desire_moved: int
+    current_market: MovingMarket
 
 class World:
-    def __init__(self, num_towns: int = 10):
+    def __init__(self, num_towns: int = 5, steps_in_year: int = 10, years_to_vote: int = 4):
         self.towns = [Town(1000) for _ in range(num_towns)]
-        self.steps_in_year = 10
-        self.years_to_vote = 4
-        self.current_year = 0
+        self.steps_in_year = steps_in_year
+        self.years_to_vote = years_to_vote
+        self.current_step = 0
     
     def step_world(self) -> WorldStepInfo:
-        total_number_people_moved = 0
-        towns_and_movers = []
-        
         for town in self.towns:
-            towns_and_movers.append(town.get_movers())
-        
-        for group_index, moving_group in enumerate(towns_and_movers):
-            other_towns = [town for i, town in enumerate(self.towns) if i != group_index]
+            # Boolean check if it is a taxes taxes are being taken
+            town.step_town(bool((self.current_step % self.steps_in_year) == 0))
 
-            random.shuffle(moving_group)
-            # Move by preference
-            while moving_group:
-                current_person = moving_group.pop(0)
-
-                happiest_town = self.towns[group_index]
-                happiness_score = self.towns[group_index].visit(current_person)
-
-                for town in other_towns:
-                    happiness_check = town.visit(current_person)
-                    if happiness_score < happiness_check:
-                        happiest_town = town
-                        happiness_score = happiness_check
-                
-                if happiest_town != self.towns[group_index]:
-                    total_number_people_moved += 1
-
-                happiest_town.people.append(current_person)
-                        
-
-            # Random town selection
-            # while moving_group:
-            #     current_person = moving_group.pop(0)
-            #     chosen_town = random.choice(other_towns)
-            #     chosen_town.add_person(current_person)
-            #     total_number_people_moved += 1
-        
-        if self.current_year % (self.steps_in_year * self.years_to_vote) == 0:
+        # Voting year?
+        if (self.current_step % (self.steps_in_year * self.years_to_vote)) == 0:
             for town in self.towns:
                 town.vote_for_platform()
-        
-        self.current_year += 1
 
-        return WorldStepInfo(total_number_people_moved)
-    
+        move_info = self.move_people()
+
+        self.current_step += 1
+
+        return move_info
+
+    def move_people(self):
+        base_moving_cost = 50
+        total_number_people_moved = 0
+        total_number_want_moved = 0
+
+        # Initialize Market
+        current_market = MovingMarket(base_moving_cost)
+
+        # Initialize Demand Objects
+        for town in self.towns:
+            current_market.town_demand.append(TownDemand(town))
+
+        # Initialize Moving Groups
+        moving_groups: List[MovingGroup] = []
+        for town in self.towns:
+            moving_groups.append(MovingGroup(town, town.get_movers()))
+
+        # Set Town Demand and Loss
+        for moving_group in moving_groups:
+            for td in current_market.town_demand:
+                # Count the people wanting to leave
+                if td.town == moving_group.from_town:
+                    td.num_people_leaving = len(moving_group.people)
+                    continue
+                
+                # Count the people who want to come
+                for person in moving_group.people:
+                    if person.wants_to_move(moving_group.from_town.town_platform, td.town.town_platform):
+                        td.num_people_want += 1
+                
+        # Move or Not
+        for moving_group in moving_groups:
+            other_towns = [town for town in self.towns if town != moving_group.from_town]
+            random.shuffle(other_towns)
+
+            for person in moving_group.people:
+                person_moved = False
+                for town in other_towns:
+                    if person.wants_to_move(moving_group.from_town.town_platform, town.town_platform):
+                        total_number_want_moved += 1
+                        if  person.money > current_market.get_town_moving_cost(town):
+                            moving_cost = current_market.get_town_moving_cost(town)
+                            person.money -= moving_cost
+                            town.town_bank += moving_cost
+                            total_number_people_moved += 1
+                            town.people.append(person)
+                            person_moved = True
+                            break
+                
+                # If we couldn't move them... well they stay then
+                if not person_moved:
+                    moving_group.from_town.people.append(person)
+
+        return WorldStepInfo(total_number_people_moved, total_number_want_moved, current_market)
+
     def __str__(self):
         ret_str = "WORLD STATS:\n"
 
